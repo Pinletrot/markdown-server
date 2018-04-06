14b273274b9bf3848c4aa284a24c59ee3fb15380
Hanpeng Liu <lhp.polo@gmail.com>
2018-04-06 12:46:48 -0700

    fix converter

--- markdownserver/markdown_converter.py
+++ markdownserver/markdown_converter.py
@@ -1,41 +1,54 @@
-import markdown as md
 import codecs
-import sys
 import os
-from env import *
+import sys
+
+import markdown as md
+from env import css_path, ms_encoding, markdown_dir, html_dir, html_extension
+from mdx_gfm import GithubFlavoredMarkdownExtension as gfme
+
+HTML_HEADER = '''
+<html>
+<head>
+<style type='text/css'>
+<!--
+''' + codecs.open(css_path, encoding=ms_encoding, mode='r').read() + \
+'''
+//-->
+</style>
+<script
+    type="text/javascript"
+    src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.4/MathJax.js">
+</script>
+</head>
+<body>
+<div class='markdown-body'>
+'''
+HTML_FOOTER = '''
+</div>
+</body>
+</html>
+'''
+
 
 class MarkdownConverter(object):
-
     def __init__(self):
-        css = codecs.open(css_path,encoding=ms_encoding,mode='r')
-        self.html_header = '''
-            <html>
-            <head>
-            <style type='text/css'>
-            <!--
-            ''' + css.read() + \
-            '''
-            //-->
-            </style>
-            </head>
-            <body>
-            <div class='markdown-body'>
-            '''
-        self.html_footer = '''
-            </div>
-            </body>
-            </html>
-            '''
+        self.html_header = HTML_HEADER
+        self.html_footer = HTML_FOOTER
 
-    def convert(self,src,dst=""):
-        code = md.markdown(self.read_md(src), extensions=[markdown_type])
-        return self.write_html(code,src,dst)
+    def convert(self, src, dst=""):
+        code = md.markdown(self.read_md(src), extensions=[gfme()])
 
-    def read_md(self,file_name):
-        md_file = codecs.open(os.path.join(markdown_dir, file_name),encoding=ms_encoding,mode='r')
+        return self.write_html(code, src, dst)
+
+    def read_md(self, file_name):
+        md_file = codecs.open(
+            os.path.join(markdown_dir, file_name),
+            encoding=ms_encoding,
+            mode='r')
+
         return md_file.read()
 
-    def write_html(self,body,file_name,dst):
+    def write_html(self, body, file_name, dst):
         html_path = os.path.join(html_dir, file_name + html_extension)
 
         if dst != "":
@@ -44,18 +57,13 @@ class MarkdownConverter(object):
             os.makedirs('/'.join(html_path.replace('\\', '/').split('/')[:-1]))
         except OSError as exc:
             pass
-        
-        html_file = codecs.open(html_path,encoding=ms_encoding,mode='w')
+
+        html_file = codecs.open(html_path, encoding=ms_encoding, mode='w')
         html_file.write(self.html_header + body + self.html_footer)
+
         return html_path
 
-def main():
-    args = sys.argv
-    if len(args) != 3:
-        print('usage: convert source_md_file target_html_file')
-    else:
-        converter = MarkdownConverter()
-        converter.convert(args[1],args[2])
 
 if __name__ == '__main__':
-    main()
+    converter = MarkdownConverter()
+    print(converter.convert('notes/quick.md'))
