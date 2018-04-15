68a99f9f8849f88eb5fc9fdb4edaae1a32bafd3a
Hanpeng Liu <lhp.polo@gmail.com>
2018-04-15 00:26:54 -0700

    remove resources

--- backend/env.py
+++ backend/env.py
@@ -3,5 +3,5 @@ import os
 root_path = '/'.join(os.path.dirname(__file__).split('/')[:-1])
 resource_dir = os.path.join(root_path, 'resources')
 markdown_dir = os.path.join(resource_dir, 'markdown')
-html_dir = os.path.join(resource_dir, 'html')
+html_dir = os.path.join(root_path, 'temp')
 
