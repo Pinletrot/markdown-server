db30a86f82d3fadde2285c70841a040f72e715ab
Hanpeng Liu <lhp.polo@gmail.com>
2018-04-16 01:17:24 -0700

    [papers] change to typescript

---
+++ frontend/papers-dev/src/index.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import Vue from 'vue';
+import pitem from './components/pitem.vue';
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    papers: [{
+      title: 'initial',
+      url: '',
+      uid: 0,
+    }],
+  },
+  components: {
+    pitem,
+  },
+  created() {
+    const vm = this;
+    axios.get(
+      '/papers/get_all',
+    ).then((response) => {
+      vm.papers = response.data.papers;
+    }).catch((error) => {
+      console.log('error @ Vue created: ', error);
+    });
+  },
+});
